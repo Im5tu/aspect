0b995c2b2ecb14fff628b937e4e2c974c8b78c94
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2021-06-03 20:08:46 +0100

    Allowed displaying of policy suites

--- src/Aspect/Commands/PolicyListCommand.cs
+++ src/Aspect/Commands/PolicyListCommand.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
+using Aspect.Policies;
 using Aspect.Policies.CompilerServices;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Aspect.Commands
 {
-    internal class PolicyListCommand : Command<PolicyListCommandSettings>
+    internal sealed class PolicyListCommand : Command<PolicyListCommandSettings>
     {
         private readonly IPolicyCompiler _policyCompiler;
 
@@ -35,24 +37,49 @@ namespace Aspect.Commands
             if (!directory.EndsWith(Path.DirectorySeparatorChar))
                 directory += Path.DirectorySeparatorChar;
 
-            var searchOption = commandSettings.Recursive.GetValueOrDefault(false) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            var rows = directory.Equals("builtin", StringComparison.OrdinalIgnoreCase)
+                ? GetBuiltInPolicies()
+                : GetPoliciesFromDirectory(directory, commandSettings.Recursive.GetValueOrDefault(false) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
             var table = new Table();
             table.AddColumn("Policy");
             table.AddColumn("Resource");
             table.AddColumn("Created");
             table.AddColumn("Last Updated");
-            foreach (var policy in Directory.EnumerateFiles(directory, "*.policy", searchOption).OrderBy(x => x))
-            {
-                var fi = new FileInfo(policy);
-                var policyName = fi.FullName;
-                var resource = _policyCompiler.GetResourceForPolicyFile(policy) ?? "[italic red]<invalid policy>[/]";
-                table.AddRow(policyName, resource, fi.CreationTime.ToString("O"), fi.LastWriteTime.ToString("O"));
-            }
+            foreach (var row in rows.OrderBy(x => x.policy))
+                table.AddRow(row.policy, row.resource, row.creationDate, row.lastUpdated);
 
             if (table.Rows.Count > 0)
                 AnsiConsole.Render(table);
 
             return 0;
+        }
+
+        private IEnumerable<(string policy, string resource, string creationDate, string lastUpdated)> GetBuiltInPolicies()
+        {
+            return Enumerable.Empty<(string policy, string resource, string creationDate, string lastUpdated)>();
+        }
+
+        private IEnumerable<(string policy, string resource, string creationDate, string lastUpdated)> GetPoliciesFromDirectory(string directory, SearchOption searchOption)
+        {
+            var rows = new List<(string policy, string resource, string creationDate, string lastUpdated)>();
+            foreach (var policy in Directory.EnumerateFiles(directory, $"*{FileExtensions.PolicyFileExtension}", searchOption))
+            {
+                var fi = new FileInfo(policy);
+                var policyName = fi.FullName;
+                var resource = _policyCompiler.GetResourceForPolicyFile(policy) ?? "[italic red]Invalid Resource[/]";
+                rows.Add((policyName, resource, fi.CreationTime.ToString("O"), fi.LastWriteTime.ToString("O")));
+            }
+
+            foreach (var policy in Directory.EnumerateFiles(directory, $"*{FileExtensions.PolicySuiteExtension}", searchOption))
+            {
+                var fi = new FileInfo(policy);
+                var policyName = fi.FullName;
+                // TODO :: could we list the resources by extracting them out? This could be a lot of IO though...?
+                rows.Add((policyName, "Various", fi.CreationTime.ToString("O"), fi.LastWriteTime.ToString("O")));
+            }
+
+            return rows;
         }
     }
 }
