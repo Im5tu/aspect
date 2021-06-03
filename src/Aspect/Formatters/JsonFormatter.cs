ecebaa555a370b430641a599bfa03dc337ab930c
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2021-06-03 17:23:39 +0100

    Implemented JsonFormatter

--- src/Aspect/Formatters/JsonFormatter.cs
+++ src/Aspect/Formatters/JsonFormatter.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Spectre.Console;
 
 namespace Aspect.Formatters
 {
     internal sealed class JsonFormatter : IFormatter
     {
-        public ValueTask FormatAsync<T>(T entity)  where T : class => FormatAsync(new[] {entity});
+        private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.Indented,
+        };
+
+        public ValueTask FormatAsync<T>(T entity)  where T : class
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(entity, _settings));
+            return ValueTask.CompletedTask;
+        }
+
         public ValueTask FormatAsync<T>(IEnumerable<T> entities) where T : class
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine(JsonConvert.SerializeObject(entities, _settings));
+            return ValueTask.CompletedTask;
         }
     }
 }
